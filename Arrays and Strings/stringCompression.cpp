c83a5de24c1f5e7a34a46981bc18ea59308a364b
akash350poria <akash350poria@gmail.com>
2020-10-02 14:03:56 +0530

    Problem 6 string compression

--- Arrays and Strings/stringCompression.cpp
+++ Arrays and Strings/stringCompression.cpp
@@ -1,11 +1,42 @@
 //STRING COMPRESSION
-
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
+
+string stringCompression(string str)
+{
+  if (str.length() == 1)
+  {
+    return str + '1';
+  }
+  int counter = 1;
+  stringstream result;
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] == str[i + 1])
+    {
+      counter++;
+    }
+    else
+    {
+      result << (str[i]);
+      result << counter;
+      counter = 1;
+    }
+  }
+  if (result.str().length() > str.length())
+  {
+    return str;
+  }
+  return result.str();
+}
 
 int main()
 {
-  char str[100];
-  gets(str);
+  string s, result;
+  cin >> s;
+  result = stringCompression(s);
+  cout << result;
   return 0;
 }
